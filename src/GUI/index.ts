eb8547f490e007fb8c4c67b44ca55ceeac331ecf
Roger Pierre Reis Silva <rogerpierrereis@gmail.com>
2026-05-08 10:20:53 -0300

    feat: create GUIClientOperantion class to handle UI button interactions and service integration

--- src/GUI/index.ts
+++ src/GUI/index.ts
@@ -1,6 +1,14 @@
-const botoes: HTMLButtonElement[] = Array.from(document.querySelectorAll<HTMLButtonElement>('.botao'));
-botoes.forEach(botao => {
-    botao.addEventListener('click', () => {
-        console.log(botao.id);
-    })
-})
+import { BalanceUseCase } from "../RegrasNegocio/BalanceUseCase";
+
+
+class GUIClientOperantion {
+    constructor(
+        private butons = Array.from(document.querySelectorAll('.botao').values()),
+        private saudacao = (document.querySelector('.saudacao'))
+    ) { }
+    private onClick(services: string[]): void {
+        this.butons.find(buton => {
+
+        })
+    }
+}
